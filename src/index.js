d21bdad7d462a5463fd31bca7882f83ce1921769
Oleg <olegator2510@mail.ru>
2018-02-23 20:27:07 +0200

    100% test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var arr, a, b, c, d, x1, x2;
+  var answ = [];
+  var str = equation;
+
+str = str.replace(/\s\*\sx\^2\s/,",");
+str = str.replace(/\s/,"");
+str = str.replace(/\s\*\sx\s/,",");
+str = str.replace(/\s/,"");
+
+arr = str.split(",");
+arr = arr.map(function(item) {
+return Number(item);
+})    
+
+a = arr[0]; b = arr[1]; c = arr[2];
+
+d = b * b - 4 * a * c;
+x1 = Math.round((-b + Math.sqrt(d))/ (2 * a));
+x2 = Math.round((-b - Math.sqrt(d))/ (2 * a));
+
+(x1 > x2) ? answ.push(x2, x1) : answ.push(x1, x2);
+
+return answ;
 }
